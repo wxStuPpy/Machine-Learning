c8ffe5cfa5e70fafcc324c17430d6bd3b8fb8e58
Admin <2440580838@qq.com>
2025-09-22 16:46:52 +0800

    朴素贝叶斯

--- ML/_07_朴素贝叶斯/_01_emotion.py
+++ ML/_07_朴素贝叶斯/_01_emotion.py
@@ -4,9 +4,16 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import jieba
+import os
 
 data_df=pd.read_csv('./data.csv',encoding='utf-8-sig')
 data_df['label']=data_df['label'].map({'positive':0,'negative':1,'neutral':2})
 
+stopwords=[]
+with open('./stopwords.txt','r',encoding='utf-8-sig') as f:
+    lines=f.readline()
+    stopwords=[line.strip() for line in lines]
+
+stopwords=list(set(stopwords))
 
 
